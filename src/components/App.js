64dbcdbeb74b83eb22860fa1a99e5420e02f1931
Tyler Beck <tbeck@topofmind.com>
2019-03-16 13:53:04 -0400

    wait for images to load then show content

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ import Loading from './Loading';
 import AppRenderer from './AppRenderer';
 
 import Context from '../context';
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -20,7 +19,7 @@ class App extends Component {
   render() {
     return <div className="App">
       <Context.Provider value={this.state}>
-        <Loading />
+        <Loading context={this} />
         <AppRenderer />
       </Context.Provider>
     </div>
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,10 +3,57 @@
 import '../styles/Loading.sass'
 
 import React, {Component} from 'react';
+import * as bluebird from 'bluebird';
+
+import ImageBackground from './ImageBackground';
 
 class Loading extends Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+        return bluebird
+            .all([this._loadImages()])
+            .then(() => this.props.context.setState({isLoaded: true}));
+    }
+
+    _loadImages() {
+        const images = ImageBackground.IMAGES;
+        bluebird.map(images, image => {
+            return new Promise(resolve => {
+                const loader = this._createImgInstance();
+                loader.onload = () => {
+                    this._destroyImgInstance(loader);
+                    resolve(true);
+                };
+                loader.src = image;
+            });
+        })
+            .then(() => true);
+    }
+
+    _destroyImgInstance(src) {
+        document.body.removeChild(src);
+    }
+
+    _createImgInstance() {
+        const imgLoader = document.createElement('img');
+        if (imgLoader) {
+            imgLoader.style.display = 'none';
+            document.body.appendChild(imgLoader);
+        }
+        return imgLoader;
+    }
+
     render() {
-        return <div className="Loading">Loading ...</div>
+        return <div className="Loading">
+            <div className="project-name">Guess the whereabouts</div>
+            <div className="loading-text">Loading ...</div>
+            <div className="progress-bar"></div>
+            <div className="progress-text">
+
+            </div>
+        </div>
     }
 }
 
